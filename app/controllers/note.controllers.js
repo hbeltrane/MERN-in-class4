bd6dc9fde080e30944b0bcf0fc1fac18f07a1f5c
hbeltrane <hbeltrane@hotmail.com>
2023-03-30 16:01:59 -0400

    controller hugo

--- app/controllers/note.controllers.js
+++ app/controllers/note.controllers.js
@@ -1,5 +1,43 @@
 const Note = require('../models/note.model.js');
 
+// Create and Save a new Note
+exports.create = (req, res) => {
+    // Validate request
+    if(!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a Note
+    const note = new Note({
+        title: req.body.title || "Untitled Note", 
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Note."
+        });
+    });
+};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {
+    Note.find()
+    .then(notes => {
+        res.send(notes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    });
+};
+
 // Find a single note with a noteId
 exports.findOne = (req, res) => {
   Note.findById(req.params.noteId)
@@ -20,16 +58,6 @@ exports.findOne = (req, res) => {
           message: "Error retrieving note with id " + req.params.noteId
       });
   });
-};
-
-// Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
-
-};
-
-// Find a single note with a noteId
-exports.findOne = (req, res) => {
-
 };
 
 // Update a note identified by the noteId in the request
